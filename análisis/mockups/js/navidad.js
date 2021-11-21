2683e92fe3309b1d7d30bb9edeedf8b961c33e31
erodriguezmartinez <erodriguezmartinez.guadalupe@alumnado.fundacionloyola.net>
2021-11-21 22:28:26 +0100

    Sprint 1 - Planificación

---
+++ análisis/mockups/js/navidad.js
@@ -0,0 +1,8 @@
+/**navidad.js
+Proyecto tarjeta de navidad.
+@autor : Esperanza Rogríguez Martínez <erodriguezmartinez.guadalupe@alumnado.fundacionloyola.net>.
+@license : GPL v3 o superior.
+Año 2021
+**/
+'use strict'
+
